1e9ac27f37f2a0e70ca8db3620403d5a62eaeb87
Rory Murray <rory.christian.murray@gmail.com>
2020-02-21 14:55:39 -0500

    Create enter function

--- Lottery.sol
+++ Lottery.sol
@@ -2,9 +2,15 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address[] public players;
+    
     
     function Lottery() public {
         manager = msg.sender;
+    }
+    
+    function enter() public payable {
+        players.push(msg.sender);
     }
     
 }
