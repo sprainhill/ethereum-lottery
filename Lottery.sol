3cd2e0642cb14e765d98fe9afcba744633f3e5d6
Rory Murray <rory.christian.murray@gmail.com>
2020-02-22 13:57:20 -0500

    Create returnPlayers function

--- Lottery.sol
+++ Lottery.sol
@@ -30,5 +30,9 @@ contract Lottery {
         require(msg.sender == manager);
         _;
     }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
     
 }
